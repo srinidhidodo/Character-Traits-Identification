31b868e910a62677ad300f3bc8a1a4fcd1ec2768
srinidhidodo <srinidhi.nps@gmail.com>
2018-03-23 11:37:43 +0530

    removed redundant changes

--- createSavePreprocessed.py
+++ createSavePreprocessed.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 12 17:05:12 2018
-
-@author: Srinidhi
-"""
 
 import time
 startTime = time.time()
@@ -22,47 +17,6 @@ import string
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-def isExtroverted(s):
-    #print(s)
-    tempL = ['ESTP','ESTJ','ESFP','ESFJ','ENTP','ENTJ','ENFP','ENFJ']
-    ret = []
-    for i in s:
-        if i in tempL:
-            ret.append(True)
-        else:
-            ret.append(False)
-    return ret
-
-def isIntuition(s):
-    tempL = ['ENTP','ENTJ','ENFP','ENFJ','INTP','INTJ','INFP','INFJ']
-    ret = []
-    for i in s:
-        if i in tempL:
-            ret.append(True)
-        else:
-            ret.append(False)
-    return ret
-
-def isThinking(s):
-    tempL = ['ENTP','ENTJ','ESTP','ESTJ','INTP','INTJ','ISTP','ISTJ']
-    ret = []
-    for i in s:
-        if i in tempL:
-            ret.append(True)
-        else:
-            ret.append(False)
-    return ret
-
-def isJudging(s):
-    tempL = ['ENFJ','ENTJ','ESTJ','ESFJ','INFJ','INTJ','ISTJ','ISFJ']
-    ret = []
-    for i in s:
-        if i in tempL:
-            ret.append(True)
-        else:
-            ret.append(False)
-    return ret
-
 fil = list(csv.reader(open('mbti_big5scores_LIWC.csv', encoding = 'utf8')))
 vocabFile = open('top500vocab_bigrams.txt','r')
 tknzr = TweetTokenizer()
@@ -73,18 +27,7 @@ countVect = CountVectorizer()
 
 vocab = set()
 stopWords = set(stopwords.words('english'))
-features = {}
-textTrack = {}
 puncts = set(string.punctuation)
-
-nSamples = 6000 #For MBTI classes
-nHiddenL1 = 1000
-nHiddenL2 = 200
-#nHiddenL3 = 20
-lr = 0.001
-#testSize = 5 #For MBTI classes
-
-rownum = 0
 
 vocab = set(vocabFile.read().strip().split('\n'))
 nWords = len(vocab) #Size of input layer = length of vocabulary
