54fb442630a2a1f1b39d536caae4aa91e204ffe0
srinidhidodo <srinidhi.nps@gmail.com>
2018-04-19 16:53:20 +0530

    updated

--- createSavePreprocessed.py
+++ createSavePreprocessed.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 17:05:12 2018
+
+@author: Srinidhi
+"""
 
 import time
 startTime = time.time()
@@ -17,8 +22,49 @@ import string
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
+def isExtroverted(s):
+    #print(s)
+    tempL = ['ESTP','ESTJ','ESFP','ESFJ','ENTP','ENTJ','ENFP','ENFJ']
+    ret = []
+    for i in s:
+        if i in tempL:
+            ret.append(True)
+        else:
+            ret.append(False)
+    return ret
+
+def isIntuition(s):
+    tempL = ['ENTP','ENTJ','ENFP','ENFJ','INTP','INTJ','INFP','INFJ']
+    ret = []
+    for i in s:
+        if i in tempL:
+            ret.append(True)
+        else:
+            ret.append(False)
+    return ret
+
+def isThinking(s):
+    tempL = ['ENTP','ENTJ','ESTP','ESTJ','INTP','INTJ','ISTP','ISTJ']
+    ret = []
+    for i in s:
+        if i in tempL:
+            ret.append(True)
+        else:
+            ret.append(False)
+    return ret
+
+def isJudging(s):
+    tempL = ['ENFJ','ENTJ','ESTJ','ESFJ','INFJ','INTJ','ISTJ','ISFJ']
+    ret = []
+    for i in s:
+        if i in tempL:
+            ret.append(True)
+        else:
+            ret.append(False)
+    return ret
+
 fil = list(csv.reader(open('mbti_big5scores_LIWC.csv', encoding = 'utf8')))
-vocabFile = open('top500vocab_bigrams.txt','r')
+vocabFile = open('top500vocab.txt','r')
 tknzr = TweetTokenizer()
 #sentAn = SentimentIntensityAnalyzer() #sentiment analyzer
 lancaster = LancasterStemmer() #PorterStemmer()
@@ -27,11 +73,22 @@ countVect = CountVectorizer()
 
 vocab = set()
 stopWords = set(stopwords.words('english'))
+features = {}
+textTrack = {}
 puncts = set(string.punctuation)
 
-vocab = set(vocabFile.read().strip().split('\n'))
-nWords = len(vocab) #Size of input layer = length of vocabulary
+nSamples = 6000 #For MBTI classes
+nHiddenL1 = 1000
+nHiddenL2 = 200
+#nHiddenL3 = 20
+lr = 0.001
+#testSize = 5 #For MBTI classes
 
+rownum = 0
+
+vocab = set(vocabFile.read().strip().split(' '))
+nWords = len(vocab) #Size of input layer = length of vocabulary
+print(len(vocab), vocab)
 dataTemp = []
 
 endTime = time.time()
@@ -44,13 +101,14 @@ fil = fil[1:]
 liwcTemp = []
 #print(len(liwcTemp))
 
-'''
+
 for j in fil:
-    i = j[1]
+    i = temp1 = j[1]
     temp1 = i.split('|||')
+    #temp1 = i
     temp2 = []
     for i in temp1:
-        #Removing URLs and numbers
+    #Removing URLs and numbers
         x = re.sub(r'https?\S+', '', re.sub(r'\w*\d\w*', '', i).strip()).strip()
         #Tokenizing
         tok = list(tknzr.tokenize(x))
@@ -59,15 +117,15 @@ for j in fil:
             tempWord = ''.join(ch for ch in tempWord if ch not in puncts)
             if len(tempWord) > 1 and tempWord in vocab:
                 temp2.append(tempWord)
-    dataToAdd = [j[0], ' '.join(temp2)]
+    dataToAdd = [j[0], ' '.join(temp2)] + j[2:7]
     dataTemp.append(dataToAdd)
 #    for i in range(len(j[8:])):
 #        liwcTemp[i].append(j[i+8])
-    liwcTemp.append([np.float(k) for k in j[16:40]])
+    liwcTemp.append([np.float(k) for k in j[8:]])
 endTime = time.time()
 print('Preprocessing time:', str(endTime - startTime))
-'''
 
+'''
 for j in fil:
     i = j[1]
     temp1 = i.split('|||')
@@ -95,10 +153,7 @@ for j in fil:
     liwcTemp.append([np.float(k) for k in j[16:40]])
 endTime = time.time()
 print('Preprocessing time:', str(endTime - startTime))
+'''
 
-np.save('16to40withliwc_bigrams.npy', dataTemp)
-np.save('16to40ofliwc_bigrams.npy', liwcTemp)
-
-
-
-
+np.save('16to40withliwc_twitter_big5.npy', dataTemp)
+np.save('16to40ofliwc_twitter.npy', liwcTemp)
