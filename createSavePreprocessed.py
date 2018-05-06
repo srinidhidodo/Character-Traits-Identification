9b217b1afc8f212f4516f1d4626f980f38be6c8f
srinidhidodo <srinidhi.nps@gmail.com>
2018-05-06 13:03:34 +0530

    Add files via upload

--- createSavePreprocessed.py
+++ createSavePreprocessed.py
@@ -66,46 +66,24 @@ def isJudging(s):
 fil = list(csv.reader(open('mbti_big5scores_LIWC.csv', encoding = 'utf8')))
 vocabFile = open('top500vocab.txt','r')
 tknzr = TweetTokenizer()
-#sentAn = SentimentIntensityAnalyzer() #sentiment analyzer
 lancaster = LancasterStemmer() #PorterStemmer()
 wordnetlem = WordNetLemmatizer()
 countVect = CountVectorizer()
 
 vocab = set()
 stopWords = set(stopwords.words('english'))
-features = {}
-textTrack = {}
 puncts = set(string.punctuation)
-
-nSamples = 6000 #For MBTI classes
-nHiddenL1 = 1000
-nHiddenL2 = 200
-#nHiddenL3 = 20
-lr = 0.001
-#testSize = 5 #For MBTI classes
-
-rownum = 0
 
 vocab = set(vocabFile.read().strip().split(' '))
 nWords = len(vocab) #Size of input layer = length of vocabulary
-print(len(vocab), vocab)
 dataTemp = []
 
-endTime = time.time()
-print('Importing and setup time:', str(endTime - startTime))
-
-print('Starting preprocessing')
-startTime = time.time()
 fil = fil[1:]
-#liwcTemp = [[] for i in fil[0][8:]]
 liwcTemp = []
-#print(len(liwcTemp))
-
 
 for j in fil:
     i = temp1 = j[1]
     temp1 = i.split('|||')
-    #temp1 = i
     temp2 = []
     for i in temp1:
     #Removing URLs and numbers
@@ -119,41 +97,8 @@ for j in fil:
                 temp2.append(tempWord)
     dataToAdd = [j[0], ' '.join(temp2)] + j[2:7]
     dataTemp.append(dataToAdd)
-#    for i in range(len(j[8:])):
-#        liwcTemp[i].append(j[i+8])
     liwcTemp.append([np.float(k) for k in j[8:]])
-endTime = time.time()
-print('Preprocessing time:', str(endTime - startTime))
 
-'''
-for j in fil:
-    i = j[1]
-    temp1 = i.split('|||')
-    temp2 = []
-    for i in temp1:
-        #Removing URLs and numbers
-        x = re.sub(r'https?\S+', '', re.sub(r'\w*\d\w*', 'thisisalink', i).strip()).strip()
-        #Tokenizing
-        tok = list(tknzr.tokenize(x))
-        for x in tok:
-            tempWord = lancaster.stem(wordnetlem.lemmatize(x.lower()))
-            tempWord = ''.join(ch for ch in tempWord if ch not in puncts)
-            if len(tempWord) > 1:
-                temp2.append(tempWord)
-    #dataToAdd = [j[0], ' '.join(temp2)]
-    temp2 = bigrams(temp2)
-    dataToAdd = []
-    for i in temp2:
-        k = ' '.join(i)
-        if k in vocab:
-            dataToAdd.append(''.join(i))
-    dataTemp.append([j[0],' '.join(dataToAdd)])
-#    for i in range(len(j[8:])):
-#        liwcTemp[i].append(j[i+8])
-    liwcTemp.append([np.float(k) for k in j[16:40]])
-endTime = time.time()
-print('Preprocessing time:', str(endTime - startTime))
-'''
 
-np.save('16to40withliwc_twitter_big5.npy', dataTemp)
-np.save('16to40ofliwc_twitter.npy', liwcTemp)
+np.save('16to40withliwc.npy', dataTemp)
+np.save('16to40ofliwc.npy', liwcTemp)
